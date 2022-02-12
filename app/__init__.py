53b59db4b251523865bb7172893bf4cfe011f116
hpuerta <hyyyp@hotmail.com>
2022-02-12 16:05:57 -0500

    Added testing variable

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
+from cgi import test
 from flask import Flask
 import os
 
-def create_app():
+def create_app(test_config=None):
     app = Flask(__name__)
     app.config.from_mapping(
         TEST_VAR = os.environ['TEST_VAR'],
         API_KEY = os.environ['API_KEY']
     )
+
+    if test_config is not None:
+        app.config.from_mapping(test_config)
+        
     return app
--- test/test_app.py
+++ test/test_app.py
@@ -12,3 +12,10 @@ def test_get_enviroment_variables_as_config():
         assert app.config['TEST_VAR']=="my_value"
     except Exception as e:
         assert False,e
+CONFIG = {'TESTING' : True}
+def test_set_testing_config():
+    try:
+        app = create_app(CONFIG)
+        assert app.config['TESTING']==True
+    except Exception as e:
+        assert False,e
