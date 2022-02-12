9bfff5331e85973b2b00580982abb7cc4ee97fd9
hpuerta <hyyyp@hotmail.com>
2022-02-12 16:01:35 -0500

    Enviroment variables setted

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import os
 
 def create_app():
     app = Flask(__name__)
-    
+    app.config.from_mapping(
+        TEST_VAR = os.environ['TEST_VAR'],
+        API_KEY = os.environ['API_KEY']
+    )
     return app
--- test/test_app.py
+++ test/test_app.py
@@ -4,3 +4,11 @@ def test_first_import():
         assert True
     except Exception as e:
         assert False,e
+
+from app import create_app
+def test_get_enviroment_variables_as_config():
+    try:
+        app = create_app()
+        assert app.config['TEST_VAR']=="my_value"
+    except Exception as e:
+        assert False,e
