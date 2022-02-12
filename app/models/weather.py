abbe2cf501b311642b5a7d8ab835fd4c198a05e1
hpuerta <hyyyp@hotmail.com>
2022-02-12 16:45:45 -0500

    Api basic data getted

--- app/models/weather.py
+++ app/models/weather.py
@@ -1,5 +1,7 @@
 import os
 import requests
+from datetime import datetime
+
 class Weather():
     __city = ""
     __country = ""
@@ -15,3 +17,19 @@ class Weather():
     
     def getWeatherJson(self):
         self.requestWeatherJson = requests.get(f"{self.__API_URL_WEATHER}").json()
+
+    def getResponseData(self):
+        self.getWeatherJson()
+        answerToResponse = {   
+                "location_name": self.query,
+                "temperature": self.requestWeatherJson['main']['temp'],
+                "wind": str(self.requestWeatherJson['wind']['speed']) + "," + str(self.requestWeatherJson['wind']['deg']),
+                "cloudiness": self.requestWeatherJson['clouds']['all'],
+                "pressure": self.requestWeatherJson['main']['pressure'],
+                "humidity": self.requestWeatherJson['main']['humidity'],
+                "sunrise": self.requestWeatherJson['sys']['sunrise'],
+                "sunset": self.requestWeatherJson['sys']['sunset'],
+                "geo_coordinates": str(self.requestWeatherJson['coord']['lat']) + "," + str(self.requestWeatherJson['coord']['lon']),
+                "requested_time": datetime.utcnow()
+            }
+        return answerToResponse
--- test/test_weather.py
+++ test/test_weather.py
@@ -5,6 +5,7 @@ def test_import_weather():
     except Exception as e:
         assert False,e
 
+from calendar import LocaleHTMLCalendar
 from app.models.weather import Weather
 def test_weather_init():
     try:
@@ -25,5 +26,22 @@ def test_get_weather_api_data_json():
         response = weather.requestWeatherJson
         assert "visibility" in response.keys()
         assert response["name"] == "Bogota"
+    except Exception as e:
+        assert False,e
+
+def test_get_now_data_basic_formated():
+    try:
+        weather = Weather(**LOCATION)
+        response = weather.getResponseData()
+        assert "location_name" in response.keys()
+        assert "temperature" in response.keys()
+        assert "wind" in response.keys()
+        assert "cloudiness" in response.keys()
+        assert "pressure" in response.keys()
+        assert "humidity" in response.keys()
+        assert "sunrise" in response.keys()
+        assert "sunset" in response.keys()
+        assert "geo_coordinates" in response.keys()
+        assert "requested_time" in response.keys()
     except Exception as e:
         assert False,e
