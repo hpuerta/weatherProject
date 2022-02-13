5f6c32ff7cabfbdd62916cd0cfe813cbe784e67f
hpuerta <hyyyp@hotmail.com>
2022-02-12 21:08:46 -0500

    Redis cache added

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask,request,jsonify
 import os
+from flask_caching import Cache
 
 from app.models.weather import Weather
 
@@ -9,17 +10,23 @@ def create_app(test_config=None):
         TEST_VAR = os.environ['TEST_VAR'],
         API_KEY = os.environ['API_KEY'],
         MOCKED_WEATHER = None,
-        MOCKED_FORECAST = None
+        MOCKED_FORECAST = None,
+        CACHE_TYPE = os.environ['CACHE_TYPE'],
+        CACHE_REDIS_HOST = os.environ['CACHE_REDIS_HOST'],
+        CACHE_REDIS_PORT = os.environ['CACHE_REDIS_PORT'],
+        CACHE_REDIS_DB = os.environ['CACHE_REDIS_DB'],
+        CACHE_REDIS_URL = os.environ['CACHE_REDIS_URL'],
+        CACHE_DEFAULT_TIMEOUT = os.environ['CACHE_DEFAULT_TIMEOUT']
     )
     
     if test_config is not None:
         app.config.from_mapping(test_config)
-    
+    cache = Cache(app)
     @app.route("/weather",methods=['GET'])
+    @cache.cached(timeout=120, query_string=True)
     def get_weather():
         country = request.args.get('country')
         city = request.args.get('city')
-        #return jsonify(app.config['MOCKED_WEATHER']),200
         weather = Weather(city,country,mocked_weather_response_url=app.config['MOCKED_WEATHER'],mocked_forecast_response_url=app.config['MOCKED_FORECAST'])
         answerToResponse = weather.getResponseData()
         return jsonify(answerToResponse),200
